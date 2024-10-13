f24c28a7d55cc25f253c25547057553b6052030b
Amilton <amiltonjose.pe@gmail.com>
2024-10-13 16:33:27 -0300

    refactor: atualização de explicações

--- src/Pasta1/TiposDeVariaveis.java
+++ src/Pasta1/TiposDeVariaveis.java
@@ -10,12 +10,13 @@ package Pasta1;
 public class TiposDeVariaveis {
     /**
      * 3) Variável estática
-     * Uma variável que é declarada como estática é chamada de variável estática. Ela não pode ser local. Você pode criar uma única cópia da variável estática e compartilhá-la entre todas as instâncias da classe.
-     * A alocação de memória para variáveis ​​estáticas acontece apenas uma vez quando a classe é carregada na memória.
+     * Uma variável que é declarada como static é chamada de variável estática. Ela não pode ser local.
+     * Você pode criar uma única cópia da variável estática e compartilhá-la entre todas as instâncias da classe.
+     * A alocação de memória para variáveis estáticas acontece apenas uma vez quando a classe é carregada na memória.
      */
     static int m = 100; // Variável estática
     /**
-     * 1) Variável local
+     * 1) Variável local:
      * Uma variável declarada dentro do corpo do método é chamada de variável local.
      * Você pode usar essa variável somente dentro daquele método e os outros métodos na classe nem sequer sabem que a variável existe.
      * Uma variável local não pode ser definida com a palavra-chave "static".
